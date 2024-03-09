f38803c0856e84c992f3bbc34f7631770f99f2fe
Pavel188 <jarekjarecki188@gmail.com>
2024-03-09 13:39:20 +0100

    first commit

---
+++ dist/5345255ae96bd0ab7c38.bundle.js
@@ -0,0 +1,115 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/sum2.js":
+/*!*********************!*\
+  !*** ./src/sum2.js ***!
+  \*********************/
+/***/ ((module) => {
+
+function sum(a,b){
+    return a+b;
+}
+
+module.exports = {
+    sum
+}
+
+/***/ }),
+
+/***/ "./src/sum3.js":
+/*!*********************!*\
+  !*** ./src/sum3.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "sum3": () => (/* binding */ sum3)
+/* harmony export */ });
+function sum3(a,b,c){
+    return a+b+c;
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _sum3__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./sum3 */ "./src/sum3.js");
+console.log(`Hello, world!`);
+
+const sumFile = __webpack_require__(/*! ./sum2 */ "./src/sum2.js");
+
+console.log(sumFile.sum(1,2));
+console.log((0,_sum3__WEBPACK_IMPORTED_MODULE_0__.sum3)(1,2,3));
+
+let  title = document.querySelector('#demo');
+title.innerHTML = `wynik: ${sumFile.sum(1,2)}`;
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=5345255ae96bd0ab7c38.bundle.js.map
